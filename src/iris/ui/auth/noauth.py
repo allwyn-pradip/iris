1d61d4cee2bfaec98ba95140a85433c789c905f1
allwyn-pradip <allwyn.pradip@freshworks.com>
2022-03-14 14:19:32 +0530

    auth and freshping update

--- src/iris/ui/auth/noauth.py
+++ src/iris/ui/auth/noauth.py
@@ -1,6 +1,7 @@
 # Copyright (c) LinkedIn Corporation. All rights reserved. Licensed under the BSD-2 Clause license.
 # See LICENSE in the project root for license information.
 
+
 class Authenticator:
     def __init__(self, config):
         pass
